21126166d52fa8f539c7b35e2333fd535cfaf683
PG_Code <2512526656@qq.com>
2024-05-28 11:51:17 +0800

    初始化项目

---
+++ src/main/java/com/pg_code/yoyochatroom/YoyoChatRoomApplication.java
@@ -0,0 +1,13 @@
+package com.pg_code.yoyochatroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoyoChatRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoyoChatRoomApplication.class, args);
+    }
+
+}
